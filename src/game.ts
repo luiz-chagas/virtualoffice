7f123c3da9a10c3c51355d27d365b566699d88b0
Luiz Chagas <luizchagasjr@gmail.com>
2020-12-14 11:58:23 -0600

    Update game
    - Added wooden floor
    - Added player character
    - Added camera and world collision

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,167 @@
-import 'phaser';
+import "phaser";
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
+let player: Phaser.Physics.Arcade.Sprite;
+let cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
+export default class GameScene extends Phaser.Scene {
+  constructor() {
+    super("gameScene");
+    Phaser.Scene.call(this, { key: "gameScene", active: true });
+  }
+
+  preload() {
+    this.load.image("ground-wood", "assets/wood.png");
+    this.load.atlas(
+      "atlas",
+      "assets/player/atlas.png",
+      "assets/player/atlas.json"
+    );
+  }
+
+  create() {
+    this.add
+      .tileSprite(
+        0,
+        0,
+        this.physics.world.bounds.width,
+        this.physics.world.bounds.height,
+        "ground-wood"
+      )
+      .setOrigin(0)
+      .setTileScale(0.1);
+
+    player = this.physics.add
+      .sprite(100, 100, "atlas", "misa-front")
+      .setCollideWorldBounds(true)
+      .setSize(30, 40)
+      .setOffset(0, 24);
+
+    const anims = this.anims;
+    anims.create({
+      key: "misa-left-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-left-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    anims.create({
+      key: "misa-right-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-right-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    anims.create({
+      key: "misa-front-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-front-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    anims.create({
+      key: "misa-back-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-back-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    const camera = this.cameras.main
+      .startFollow(player)
+      .setZoom(2)
+      .setBounds(
+        0,
+        0,
+        this.physics.world.bounds.width,
+        this.physics.world.bounds.height
+      );
+
+    cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update(time, delta) {
+    const speed = 175;
+    const prevVelocity = player.body.velocity.clone();
+
+    const body = player.body as Phaser.Physics.Arcade.Body;
+
+    // Stop any previous movement from the last frame
+    body.setVelocity(0);
+
+    // Horizontal movement
+    if (cursors.left.isDown) {
+      body.setVelocityX(-speed);
+    } else if (cursors.right.isDown) {
+      body.setVelocityX(speed);
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    // Vertical movement
+    if (cursors.up.isDown) {
+      body.setVelocityY(-speed);
+    } else if (cursors.down.isDown) {
+      body.setVelocityY(speed);
     }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+    // Normalize and scale the velocity so that player can't move faster along a diagonal
+    body.velocity.normalize().scale(speed);
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+    // Update the animation last and give left/right animations precedence over up/down animations
+    if (cursors.left.isDown) {
+      player.anims.play("misa-left-walk", true);
+    } else if (cursors.right.isDown) {
+      player.anims.play("misa-right-walk", true);
+    } else if (cursors.up.isDown) {
+      player.anims.play("misa-back-walk", true);
+    } else if (cursors.down.isDown) {
+      player.anims.play("misa-front-walk", true);
+    } else {
+      player.anims.stop();
 
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+      // If we were moving, pick and idle frame to use
+      if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+      else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+      else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+      else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
     }
+  }
 }
 
-const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
-};
-
-const game = new Phaser.Game(config);
+new Phaser.Game({
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.ScaleModes.FIT,
+    parent: "gameScene",
+    autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+    width: "100%",
+    height: "100%",
+  },
+  backgroundColor: "#000",
+  scene: GameScene,
+  banner: {
+    hidePhaser: true,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+      height: 1000,
+      width: 1000,
+    },
+  },
+});
