13e7b1425460ba8f9221f4f87d9d1364b3693814
Luiz Chagas <luizchagasjr@gmail.com>
2020-12-21 15:22:44 -0600

    Update packages for production build

--- phaser/rollup.config.dist.js
+++ phaser/rollup.config.dist.js
@@ -1,5 +1,5 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
