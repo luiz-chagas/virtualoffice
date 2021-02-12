4f957f7881e14d706a18585139c8e398cbc846a1
Luiz Chagas <luizchagasjr@gmail.com>
2021-02-12 15:31:01 -0600

    Update localStorage.ts

--- office/src/utils/localStorage.ts
+++ office/src/utils/localStorage.ts
@@ -3,4 +3,11 @@ type Props = "char" | "playSound" | "uid";
 export const save = (x: Props, data: string | boolean) =>
   localStorage.setItem(x, JSON.stringify(data));
 
-export const load = (x: Props) => JSON.parse(localStorage.getItem(x) ?? "");
+export const load = (x: Props) => {
+  try {
+    const value = JSON.parse(localStorage.getItem(x) ?? "");
+    return value;
+  } catch (err) {
+    return null;
+  }
+};
